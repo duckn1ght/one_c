337850699b9e78fd26c9dc13b4b106e955823508
duckn1ght <duckn1ght@gmail.com>
2024-05-17 14:28:39 +0600

    Add frontend

--- routes.js
+++ routes.js
@@ -1,16 +1,27 @@
 const productController = require("./db/Products/controller");
 
 module.exports = function routes(app) {
-    app.get("/products", async (req, res) => {
+    // Главная страница
+    app.get("/", (req, res) => {
+        res.render("index", {
+            title: "Интеграция 1С",
+            one_c_url: process.env.ONE_C_URL,
+        });
+    });
+
+    // Вывод всех товаров
+    app.get("/products/", async (req, res) => {
         const products = await productController.getProducts();
         res.send(products);
     });
 
+    // Вывод определенного товара по артикулу
     app.get("/products/:code", async (req, res) => {
         const product = await productController.getProduct(req.params.code);
         res.send(product);
     });
 
+    // Добавление товара
     app.post("/products/create", async (req, res) => {
         const product = await productController.createProduct(req.body);
         res.send(product);
